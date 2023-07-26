fe3f2341f3ee1080e149f32d04fbf0450d9709e2
saka1029 <saka1029@gmail.com>
2023-07-27 00:01:26 +0900

    カスタム検索削除

--- menu.js
+++ menu.js
@@ -15,16 +15,14 @@
 //    console.log("年度=" + 年度 + " 点数表=" + 点数表 + " 種類=" + 
 //        + " 最新版=" + (年度 == 最新年度) + " 前年度=" + PREV[年度]);
     var links = "";
-//    links += "<script async src='https://cse.google.com/cse.js?cx=a6474cacc7567404b'></script>"
-//        + "<div class='gcse-search'></div><br>";
-    links += ""
-        + "<form id='cse-search-box' action='http://google.com/cse'>\n"
-        + "    <input type='hidden' name='cx' value='a6474cacc7567404b' />\n"
-        + "    <input type='hidden' name='ie' value='UTF-8' />\n"
-        + "    <input type='text' name='q' size='50' placeholder='サイト内検索' />\n"
-        + "    <input type='submit' name='sa' value='検索' />\n"
-        + "    <img src='http://www.google.com/cse/images/google_custom_search_smwide.gif' align='middle'>\n"
-        + "</form>\n";
+//    links += ""
+//        + "<form id='cse-search-box' action='http://google.com/cse'>\n"
+//        + "    <input type='hidden' name='cx' value='a6474cacc7567404b' />\n"
+//        + "    <input type='hidden' name='ie' value='UTF-8' />\n"
+//        + "    <input type='text' name='q' size='50' placeholder='サイト内検索' />\n"
+//        + "    <input type='submit' name='sa' value='検索' />\n"
+//        + "    <img src='http://www.google.com/cse/images/google_custom_search_smwide.gif' align='middle'>\n"
+//        + "</form>\n";
     links += " <a href='../../index.html' target='_top'>ホーム</a>";
     if (点数表 == "k") {
         links += " <a href='index.html'>告示</a>";
